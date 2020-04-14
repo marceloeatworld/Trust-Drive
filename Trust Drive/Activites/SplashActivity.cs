dcb941108a07d21fbe03b8a69a134136acc7e93d
marcelo pereira <yanniseatworld@live.fr>
2020-04-14 02:13:15 +0200

    first

---
+++ Trust Drive/Activites/SplashActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Trust_Drive.Activites
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = false, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Thread.Sleep(1000);
+            // Create your application here
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(LoginActivity));
+        }
+    }
+}
---
+++ Trust Drive/DataModels/AcceptedDriver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Trust_Drive.DataModels
+{
+    public class AcceptedDriver
+    {
+        public string ID { get; set; }
+        public string fullname { get; set; }
+        public string phone { get; set; }
+    }
+}
